5d69c2b858c4064b2b63ea08cf53d7eade564b4c
ReivelTimothy <timothyreivel1@gmail.com>
2025-05-21 04:04:06 +0700

    migration fixed

--- backEnd/migrations/20250520182132-insert-auction.js
+++ backEnd/migrations/20250520182132-insert-auction.js
@@ -5,9 +5,10 @@ export default {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable('Auctions', {
       auction_id: {
-        type: Sequelize.INTEGER,
-        autoIncrement: true,
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
         primaryKey: true,
+        allowNull: false
       },
       title: { type: Sequelize.STRING, allowNull: false },
       description: { type: Sequelize.TEXT, allowNull: false },
@@ -15,11 +16,11 @@ export default {
       currentPrice: { type: Sequelize.INTEGER, allowNull: false },
       startDate: { type: Sequelize.DATE, allowNull: false },
       endDate: { type: Sequelize.DATE, allowNull: false },
-      status: { type: Sequelize.ENUM('OPEN', 'CLOSED'), allowNull: false },
-      category: { type: Sequelize.ENUM('CAR', 'MOTORCYCLE'), allowNull: false },
+      status: { type: Sequelize.STRING, allowNull: false },
+      category: { type: Sequelize.STRING, allowNull: false },
       image: { type: Sequelize.STRING },
-      vehicleId: {
-        type: Sequelize.INTEGER,
+      vehicle_id: {
+        type: Sequelize.UUID,
         references: {
           model: 'Vehicles',
           key: 'vehicle_id',
